580a3c336e35f8f8925f21e18f19c1376476813d
Yixing-Lu <yixinglu97@gmail.com>
2021-01-28 10:55:33 -0800

    finish

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var hello = require('./routes/hello');
-// Example route
-// var user = require('./routes/user');
+var project = require('./routes/project');
 
 var app = express();
 
@@ -38,8 +37,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/hello/:userName', hello.view);
-// Example route
-// app.get('/users', user.list);
+app.get('/project/:name', project.viewProject);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
